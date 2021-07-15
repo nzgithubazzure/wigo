4eebd1ce5cf1e1f9a9427fef670d577847bf5508
nzgithubazzure <87454140+nzgithubazzure@users.noreply.github.com>
2021-07-15 16:37:21 +1200

    Add files via upload

---
+++ local-index.js
@@ -0,0 +1,31 @@
+const dogs = []
+
+main();
+
+async function main() {
+    await loadDogs();
+    displayDogs();
+}
+
+async function loadDogs() {
+    const response = await fetch('/api/dogs');
+    const json = await response.json();
+
+    for (let dog of json.dogs) {
+        dogs.push(dog);
+    }
+}
+
+function displayDogs() {
+    for (let dog of dogs) {
+        addDogToUI(dog);
+    }
+}
+
+function addDogToUI(dog) {
+    const dogUI = document.createElement('a');
+    dogUI.className = 'list-group-item';
+    dogUI.innerText = dog.name;
+
+    document.getElementById('dogs-list').appendChild(dogUI);
+}
